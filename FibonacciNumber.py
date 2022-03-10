3541f424227aefa5bea7a0b127af73586aa30a28
NahomSeleshi <81203642+NahomSeleshi@users.noreply.github.com>
2022-03-10 10:56:59 +0300

    Update FibonacciNumber.py

--- FibonacciNumber.py
+++ FibonacciNumber.py
@@ -3,7 +3,6 @@ class Solution:
         memo = [False for values in range(n+1)]
         def DP(index):
             if index == 0:
-                memo[index] == 0
                 return 0
             if index == 1:
                 memo[index] == 1
